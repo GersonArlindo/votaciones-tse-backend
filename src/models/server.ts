48993aa39d2ca20944429339ad2140b921ae5c1a
GersonArlindo <gerson50039@gmail.com>
2023-06-24 21:31:01 -0600

    Creando App backend Votaciones

--- src/models/server.ts
+++ src/models/server.ts
@@ -5,22 +5,10 @@ import db from './../db/connection.db';
 import indexRoutes from "./../routes/index.routes";
 import languageRoutes from "./../routes/language.routes";
 import personaNaturalesRoutes from "./../routes/persona_natural.routes";
-import password_masterRoutes from "./../routes/password_master.routes";
-import timezoneRoutes from "./../routes/timezone.routes";
-import disqualificationRoutes from "./../routes/disqualification.routes";
-import energyProviderRoutes from "./../routes/energy_provider.routes";
-import roofRoutes from "./../routes/roof.routes";
-import sales_repRoutes from "./../routes/sales_rep.routes";
-import appointment_outcomeRoutes from "./../routes/appointment_outcome.routes";
 import modulesRoutes from "./../routes/modules.routes";
 import permissionRoutes from "./../routes/permission.routes";
 import userRoutes from "./../routes/user.routes";
 import partidosPoliticosRoutes from "./../routes/partidos_politicos.routes";
-import providerRoutes from "./../routes/provider.routes";
-import installersRoutes from "./../routes/installer.routes";
-import stateRoutes from "./../routes/state.routes";
-import assign_appointmentRoutes from "./../routes/assign_appointment.routes";
-import leadRoutes from "./../routes/lead.routes";
 import roleRoutes from '../routes/role.routes';
 import authPermission from "./../routes/auth.routes"
 import uploadImagesPermission from "./../routes/upload_images.routes"
@@ -36,22 +24,10 @@ class Server {
         index: '/',
         language: '/language',
         persona_natural: '/persona-natural',
-        password_master: '/password_master',
-        timezone: '/timezone',
-        disqualification: '/disqualification',
-        energyProvider: '/energy_provider',
-        roof: '/roof',
-        sales_rep: '/sales_rep',
-        appointment_outcome:'/appointment_outcome',
         modules: '/module',
         permission: '/permission',
         user: '/user',
         partidosPoliticos: '/partidos_politicos',
-        provider: '/provider',
-        installers: '/installers',
-        state: '/state',
-        assign_appointment: '/assign_appointment',
-        lead: '/lead',
         role: '/role',
         auth: '/auth',
         upload_images: '/upload-images',
@@ -92,22 +68,10 @@ class Server {
         this.app.use(this.apiPaths.index, indexRoutes);
         this.app.use(this.apiPaths.language, languageRoutes);
         this.app.use(this.apiPaths.persona_natural, personaNaturalesRoutes);
-        this.app.use(this.apiPaths.password_master, password_masterRoutes);
-        this.app.use(this.apiPaths.timezone, timezoneRoutes);
-        this.app.use(this.apiPaths.disqualification, disqualificationRoutes);
-        this.app.use(this.apiPaths.energyProvider, energyProviderRoutes);
-        this.app.use(this.apiPaths.roof, roofRoutes);
-        this.app.use(this.apiPaths.sales_rep, sales_repRoutes);
-        this.app.use(this.apiPaths.appointment_outcome, appointment_outcomeRoutes);
         this.app.use(this.apiPaths.modules, modulesRoutes);
         this.app.use(this.apiPaths.permission, permissionRoutes);
         this.app.use(this.apiPaths.user, userRoutes);
         this.app.use(this.apiPaths.partidosPoliticos, partidosPoliticosRoutes);
-        this.app.use(this.apiPaths.provider, providerRoutes);
-        this.app.use(this.apiPaths.installers, installersRoutes);
-        this.app.use(this.apiPaths.state, stateRoutes);
-        this.app.use(this.apiPaths.assign_appointment, assign_appointmentRoutes);
-        this.app.use(this.apiPaths.lead, leadRoutes);
         this.app.use(this.apiPaths.role, roleRoutes);
         this.app.use(this.apiPaths.auth, authPermission);
         this.app.use(this.apiPaths.upload_images, uploadImagesPermission)
